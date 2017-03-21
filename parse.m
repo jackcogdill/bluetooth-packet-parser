d1640057693a8ba73c314e735be2bc5155cb8cd2
Jack Cogdill <jackcogdill@users.noreply.github.com>
2017-03-21 11:05:03 -0400

    Consolidated pdu functions into one file

--- parse.m
+++ parse.m
@@ -2,34 +2,12 @@
 
 # Includes
 source "util.m"
+source "pdu.m"
 
 # Load variable from file
 function data = load_var(file, var)
 	temp = load(file, var);
 	data = temp.(var);
-endfunction
-
-function type_str = get_pdu_type(type)
-	type = fliplr(type);
-	type = mat2dec(type);
-	switch (type)
-		case 0
-			type_str = "ADV_IND";
-		case 1
-			type_str = "ADV_DIRECT_IND";
-		case 2
-			type_str = "ADV_NONCONN_IND";
-		case 3
-			type_str = "SCAN_REQ";
-		case 4
-			type_str = "SCAN_RSP";
-		case 5
-			type_str = "CONNECT_REQ";
-		case 6
-			type_str = "ADV_SCAN_IND";
-		otherwise
-			type_str = "ERROR";
-	endswitch
 endfunction
 
 # Main function
--- pdu.m
+++ pdu.m
@@ -1,3 +1,29 @@
+# Includes
+source "util.m"
+
+function type_str = get_pdu_type(type)
+	type = fliplr(type);
+	type = mat2dec(type);
+	switch (type)
+		case 0
+			type_str = "ADV_IND";
+		case 1
+			type_str = "ADV_DIRECT_IND";
+		case 2
+			type_str = "ADV_NONCONN_IND";
+		case 3
+			type_str = "SCAN_REQ";
+		case 4
+			type_str = "SCAN_RSP";
+		case 5
+			type_str = "CONNECT_REQ";
+		case 6
+			type_str = "ADV_SCAN_IND";
+		otherwise
+			type_str = "ERROR";
+	endswitch
+endfunction
+
 function parse_pdu(type, payload)
 	switch (type)
 		case "ADV_IND"
